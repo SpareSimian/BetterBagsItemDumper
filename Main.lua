ab4874fe78d38aea2cc32e523a428e96438ae58c
Kenneth Porter <shiva+git@sewingwitch.com>
2024-03-23 10:55:21 -0700

    Need to deepcopy the table so we see it at the time the filter is called, not on logout

--- Main.lua
+++ Main.lua
@@ -4,6 +4,22 @@ LibStub('AceAddon-3.0'):NewAddon(addon, addonName, 'AceConsole-3.0')
 local bb = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
 local categories = bb:GetModule('Categories')
 
+-- deepcopy from http://lua-users.org/wiki/CopyTable
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
 categories:RegisterCategoryFunction("ItemDumper", function (data)
 
   if C_TooltipInfo and C_TooltipInfo.GetOwnedItemByID then
@@ -12,7 +28,7 @@ categories:RegisterCategoryFunction("ItemDumper", function (data)
   end
 
   -- add everything to debugging table for later inspection
-  bbidDB[data.itemInfo.itemName] = data
+  bbidDB[data.itemInfo.itemID] = deepcopy(data)
 
   return nil
 end)
